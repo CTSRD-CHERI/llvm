dd862e17bcce9d419217edc9e86727c186d17aca
David Chisnall <theraven@theravensnest.org>
2012-12-06 12:07:46 +0000

    Fix LLVM to parse / generate the new asm syntax. Fix the encoding of cld to reflect Mike's change.
    Conflicts:
    	lib/Target/Mips/MipsInstrInfo.td

--- lib/Target/Mips/AsmParser/MipsAsmParser.cpp
+++ lib/Target/Mips/AsmParser/MipsAsmParser.cpp
@@ -2155,14 +2155,13 @@ bool MipsAsmParser::ParseInstruction(
         Parser.EatToEndOfStatement();
         return Error(Loc, "unexpected token in argument list");
       }
-      if (getLexer().isNot(AsmToken::Plus)) {
+      if (getLexer().isNot(AsmToken::Comma)) {
         SMLoc Loc = getLexer().getLoc();
         Parser.EatToEndOfStatement();
         return Error(Loc,
             "expecting offset when parsing capability-relative address");
       }
-      Operands.push_back(MipsOperand::CreateToken("+", getLexer().getLoc()));
-      Parser.Lex();  // Eat the plus.
+      Parser.Lex();  // Eat the comma.
       if (ParseOperand(Operands, Name)) {
         SMLoc Loc = getLexer().getLoc();
         Parser.EatToEndOfStatement();
