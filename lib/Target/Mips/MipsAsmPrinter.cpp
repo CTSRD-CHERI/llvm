9726f396cbf4fda78e000d6dbe7857b144ba01d6
David Chisnall <theraven@theravensnest.org>
2013-12-25 20:39:52 +0000

    In fixing MIPS-IV, refrain from breaking everything else.

--- lib/Target/Mips/MipsAsmPrinter.cpp
+++ lib/Target/Mips/MipsAsmPrinter.cpp
@@ -705,6 +705,7 @@ void MipsAsmPrinter::PrintDebugValueComment(const MachineInstr *MI,
 extern "C" void LLVMInitializeMipsAsmPrinter() {
   RegisterAsmPrinter<MipsAsmPrinter> X(TheMipsTarget);
   RegisterAsmPrinter<MipsAsmPrinter> Y(TheMipselTarget);
+  RegisterAsmPrinter<MipsAsmPrinter> Mips4(TheMips4Target);
   RegisterAsmPrinter<MipsAsmPrinter> A(TheMips64Target);
   RegisterAsmPrinter<MipsAsmPrinter> B(TheMips64elTarget);
   RegisterAsmPrinter<MipsAsmPrinter> C(TheMipsCheriTarget);
