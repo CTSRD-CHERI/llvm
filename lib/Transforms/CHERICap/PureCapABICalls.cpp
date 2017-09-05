CTSRD-CHERI/llvm

commit ae84886a050dc44ecf886944e8b9233bc4bf49b2

Have the Cheri call cleanup pass delete values that it makes trivially dead.  This prevents it from leaving a load of random CHERI intrinsics lying around if DCE isn’t run afterwards.

lib/Transforms/CHERICap/PureCapABICalls.cpp
@@ -26,6 +26,7 @@
 #include "llvm/ADT/StringSwitch.h"
 #include "llvm/IR/CallSite.h"
 #include "llvm/IR/InstVisitor.h"
+#include "llvm/Transforms/Utils/Local.h"
 #include "llvm/Transforms/CHERICap.h"
 
 using namespace llvm;
@@ -34,6 +35,7 @@ namespace {
 class CHERICapDirectCalls : public FunctionPass,
                           public InstVisitor<CHERICapDirectCalls> {
   bool Modified = false;
+  SmallVector<Value*,32> DeadInstructions;
 
 public:
   static char ID;
@@ -54,13 +56,17 @@ public:
         dyn_cast<Function>(PtrToInt->getOperand(0)->stripPointerCasts());
     if (!Func)
       return;
+    DeadInstructions.push_back(CS.getCalledValue());
     CS.setCalledFunction(Func);
     CS.mutateFunctionType(
         cast<FunctionType>(Func->getType()->getElementType()));
   }
   bool runOnFunction(Function &F) override {
     Modified = false;
+    DeadInstructions.clear();
     visit(F);
+    for (auto *V : DeadInstructions)
+      RecursivelyDeleteTriviallyDeadInstructions(V);
     return Modified;
   }
   void getAnalysisUsage(AnalysisUsage &AU) const override {

commit 7f33a7c8b3f33df6f7d7fa9467a925c651f258cf

Reland ae84886a050dc44ecf886944e8b9233bc4bf49b2 with an additional check
Only add CS.getCalledValue() to DeadInstructions if it doesn't have any
uses. This should always be the case but bisecting showed that
ae84886a050dc44ecf886944e8b9233bc4bf49b2 broke PostgreSQL. The assertion
should allow us to track down which file was miscompiled.

lib/Transforms/CHERICap/PureCapABICalls.cpp
@@ -26,6 +26,7 @@
 #include "llvm/ADT/StringSwitch.h"
 #include "llvm/IR/CallSite.h"
 #include "llvm/IR/InstVisitor.h"
+#include "llvm/Transforms/Utils/Local.h"
 #include "llvm/Transforms/CHERICap.h"
 
 using namespace llvm;
@@ -34,6 +35,7 @@ namespace {
 class CHERICapDirectCalls : public FunctionPass,
                           public InstVisitor<CHERICapDirectCalls> {
   bool Modified = false;
+  SmallVector<Value*,32> DeadInstructions;
 
 public:
   static char ID;
@@ -54,13 +56,26 @@ public:
         dyn_cast<Function>(PtrToInt->getOperand(0)->stripPointerCasts());
     if (!Func)
       return;
+    auto *CalledValue = CS.getCalledValue();
     CS.setCalledFunction(Func);
-    CS.mutateFunctionType(
-        cast<FunctionType>(Func->getType()->getElementType()));
+    CS.mutateFunctionType(cast<FunctionType>(Func->getType()->getElementType()));
+    if (CalledValue->use_begin() == CalledValue->use_end()) {
+      DeadInstructions.push_back(CalledValue);
+    } else {
+#if !defined(NDEBUG) || defined(LLVM_ENABLE_DUMP)
+      llvm::errs() << "CALLEE: "; CalledValue->dump();
+      llvm::errs() << "Func: "; Func->dump();
+      llvm::errs() << "NUM USES: " << CalledValue->getNumUses() << "\n";
+#endif
+      assert(CalledValue->getNumUses() == 0 && "Unexpected uses");
+    }
   }
   bool runOnFunction(Function &F) override {
     Modified = false;
+    DeadInstructions.clear();
     visit(F);
+    for (auto *V : DeadInstructions)
+      RecursivelyDeleteTriviallyDeadInstructions(V);
     return Modified;
   }
   void getAnalysisUsage(AnalysisUsage &AU) const override {